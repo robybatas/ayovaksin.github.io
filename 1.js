47111b0bf8c2c191f183dbd1f1ee7497c162e1b8
robybatas <robybatas@gmail.com>
2021-07-30 12:58:36 +0700

    baru

--- 1.js
+++ 1.js
@@ -3,14 +3,14 @@ let submitUsia = document.getElementById('input-submit');
 submitUsia.addEventListener('click', function() {
     let inputAge = document.getElementById("input-age").value
     if (Number(inputAge) <= 11) {
-        alert('Usia kamu belum masuk kriteria untuk mendapatkan vaksin.')
+        let test = document.getElementById('test')
+        let next = document.getElementById('next')
+        test.innerHTML = 'Usia kamu belum masuk kriteria untuk mendapatkan vaksin.'
+        next.innerHTML = ''
     } else {
-        alert('Usia kamu sudah masuk kriteria untuk mendapatkan vaksin. Silahkan cek lokasi vaksin dan jadwal yang tersedia.')
+        let test = document.getElementById('test')
+        let next = document.getElementById('next')
+        test.innerHTML = 'Usia kamu sudah masuk kriteria untuk mendapatkan vaksin. Silahkan cek lokasi vaksin dan jadwal yang tersedia.'
+        next.innerHTML = `<a href="selectLocation.html">Lanjutkan</a>`
     }
-})
-
-let test = document.getElementById('test');
-
-test.addEventListener('click', function() {
-    alert('test')
 })
--- 2.js
+++ 2.js
@@ -1,65 +1,98 @@
 let l1 = document.getElementById('l1');
 
 l1.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 10 Agustus jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 10 Agustus jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
 
 let l2 = document.getElementById('l2');
 
 l2.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 31 Juli jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 31 Juli jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
 
 let l3 = document.getElementById('l3');
 
 l3.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 31 Juli jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 7 Agustus jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
 
 let l4 = document.getElementById('l4');
 
 l4.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 31 Juli jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 31 Juli jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
 
 let l5 = document.getElementById('l5');
 
 l5.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 31 Juli jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 1 Agustus jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
 
 let l6 = document.getElementById('l6');
 
 l6.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 31 Juli jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 31 Juli jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
 
 let l7 = document.getElementById('l7');
 
 l7.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 10 Agustus jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 10 Agustus jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
 
 let l8 = document.getElementById('l8');
 
 l8.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 31 Juli jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 1 Agustus jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
 
 let l9 = document.getElementById('l9');
 
 l9.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 10 Agustus jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 17 Agustus jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
 
 let l10 = document.getElementById('l10');
 
 l10.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 31 Juli jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 28 Agustus jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
 
 let l11 = document.getElementById('l11');
 
 l11.addEventListener('click', function() {
-    alert('Jadwal untuk vaksin adalah tanggal 10 Agustus jam 8:00-16:00')
+    let test = document.getElementById('test')
+    test.innerHTML = 'Jadwal untuk vaksin adalah tanggal 10 Agustus jam 8:00-16:00'
+    let next = document.getElementById('next')
+    next.innerHTML = `<a href="finish.html">Lanjutkan</a>`
 })
